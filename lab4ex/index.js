c5ae3e7eef352f03d1a58407d9eb46d155996c8c
willb <willb@DESKTOP-HB1K7LC>
2024-09-27 16:39:45 -0400

    final

--- lab4ex/index.js
+++ lab4ex/index.js
@@ -1,24 +1,47 @@
 const express = require('express');
 const app = express();
-const SERVER_PORT = process.env.PORT || 3000;  // Ensure PORT is uppercase, as it's a common standard.
+const SERVER_PORT = process.env.PORT || 3001;
+
+app.use(express.json());
 
 // Route for GET request on '/'
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send('Hello Lab4ex');
 });
 
-// Route for POST request on '/about'
-app.get('/about', (req, res) => {
-    res.send('About us');
+//A GET request to /hello that returns "Hello Express JS".
+app.get('/hello', (req, res) => {
+    res.send('Hello Express JS');
 });
 
+//A GET request to /user with query parameters for firstname and lastname.
 
-// Route for POST request on '/'
-app.post('/', (req, res) => {
-    res.send('POST - Hello world');
+app.get('/user', (req, res) => {
+    const firstname = req.query.firstname || 'Ryan';
+    const lastname = req.query.lastname || 'Tran';
+
+    res.json({
+        firstname: firstname,
+        lastname: lastname
+    });
 });
+
+//A POST request to /user with path parameters for firstname and lastname.
+
+// A POST request to /user with path parameters for firstname and lastname.
+app.post('/user/:firstname/:lastname', (req, res) => {
+    const { firstname, lastname } = req.params;
+    res.json({
+        firstname: firstname,
+        lastname: lastname
+    });
+});
+
 
 // Start the server
 app.listen(SERVER_PORT, () => {
     console.log(`Server is running on http://localhost:${SERVER_PORT}`);
 });
+
+//GET Request: https://comp3123.vercel.app/user?firstname=fn&lastname=ln
+//POST Request: https://comp3123.vercel.app/user/Ryan/Tran
