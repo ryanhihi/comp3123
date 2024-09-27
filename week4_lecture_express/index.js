769b7d1c0b6c5df23ce1581b6f7f107399f06bab
willb <willb@DESKTOP-HB1K7LC>
2024-09-27 15:50:15 -0400

    fixed 2nd

--- week4_lecture_express/index.js
+++ week4_lecture_express/index.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const app = express();
-const SERVER_PORT = process.env.port || 3000
+const SERVER_PORT = process.env.PORT || 3000;  // Ensure PORT is uppercase, as it's a common standard.
 
-//http://localhost:3000/
-app.get('/', (req, res)=>{
+// Route for GET request on '/'
+app.get('/', (req, res) => {
     res.send('Hello world');
-})
+});
 
-app.post('/about',(req, res)=>{
-    res.send('About us');}
-)
+// Route for POST request on '/about'
+app.post('/about', (req, res) => {
+    res.send('About us');
+});
 
-app. post('/',(req, res)=>{
+// Route for POST request on '/'
+app.post('/', (req, res) => {
     res.send('POST - Hello world');
-})
+});
 
+// Start the server
 app.listen(SERVER_PORT, () => {
-    console.log(`Server is running on http://localhost:${SERVER_PORT}`)
-})
-
+    console.log(`Server is running on http://localhost:${SERVER_PORT}`);
+});
